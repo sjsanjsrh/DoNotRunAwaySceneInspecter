9af8f8b3fca3e6275c58e1437f8951faa718147f
Sinduy <sjsanjsrh@naver.com>
2024-07-09 09:41:11 +0900

    Define behavior when the distance setting is 0 and when it is negative

--- DoNotRunAwaySceneInspecter.cs
+++ DoNotRunAwaySceneInspecter.cs
@@ -7,10 +7,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
-using static FrooxEngine.MeshEmitter;
 
 namespace DoNotRunAwaySceneInspecter
 {
@@ -27,7 +23,7 @@ namespace DoNotRunAwaySceneInspecter
           new ModConfigurationKey<bool>("enabled", "Should the mod be enabled", () => true);
         [AutoRegisterConfigKey]
         private static readonly ModConfigurationKey<float> distance =
-          new ModConfigurationKey<float>("distance", "Distance to move the worker inspector slot", () => 0.1f);
+          new ModConfigurationKey<float>("distance", "Distance to move the worker inspector slot if this value is 0 then set inspector position is in front of User", () => 0.1f);
         private static ModConfiguration Config;
 
         public static bool Enabled => Config.GetValue(enabled);
@@ -39,67 +35,79 @@ namespace DoNotRunAwaySceneInspecter
 
             new Harmony(DOMAIN_NAME).PatchAll();
         }
-    }
 
-    [HarmonyPatch(typeof(InspectorHelper), nameof(InspectorHelper.OpenInspectorForTarget), new Type[] { typeof(IWorldElement), typeof(Slot), typeof(bool) })]
-    class InspectorHelper_OpenInspectorButton_Patch
-    {
-        public static bool Prefix(
-              IWorldElement target,
-              Slot source = null,
-              bool openWorkerOnly = false)
+        [HarmonyPatch(typeof(InspectorHelper), nameof(InspectorHelper.OpenInspectorForTarget), new Type[] { typeof(IWorldElement), typeof(Slot), typeof(bool) })]
+        class InspectorHelper_OpenInspectorButton_Patch
         {
-            if (!DoNotRunAwaySceneInspecter.Enabled) { return true; }
+            public static bool Prefix(
+                  IWorldElement target,
+                  ref Slot source,
+                  bool openWorkerOnly)
+            {
+                if (!Enabled) { return true; }
 
-            if (target == null)
-                return false;
-            Slot nearestParent1 = target.FindNearestParent<Slot>();
-            User nearestParent2 = target.FindNearestParent<User>();
-            Worker worker = openWorkerOnly ? target.FindNearestParent<Worker>() : null;
-            if (worker == nearestParent1 || worker == nearestParent2)
-                worker = null;
-            if (nearestParent1 == null && nearestParent2 == null && worker == null)
-            {
-                UniLog.Warning("The target is neither on a Slot nor on an User.\n" + target.ParentHierarchyToString());
-            }
-            else
-            {
-                Slot slot1 = target.World.LocalUserSpace.AddSlot("Inspector");
-                Slot slot2 = source?.GetComponentInParents<SceneInspector>()?.Slot ?? source?.GetComponentInParents<UserInspector>()?.Slot;
-                if (worker != null)
-                    WorkerInspector.Create(slot1, worker);
-                else if (nearestParent1 != null)
+                if (Distance == 0.0) 
                 {
-                    SceneInspector sceneInspector = slot1.AttachComponent<SceneInspector>();
-                    sceneInspector.Root.Target = nearestParent1;
-                    sceneInspector.ComponentView.Target = nearestParent1;
+                    source = null;
+                    return true; 
                 }
-                else if (nearestParent2 != null)
+
+                if (target == null)
+                    return false;
+                Slot nearestParent1 = target.FindNearestParent<Slot>();
+                User nearestParent2 = target.FindNearestParent<User>();
+                Worker worker = openWorkerOnly ? target.FindNearestParent<Worker>() : null;
+                if (worker == nearestParent1 || worker == nearestParent2)
+                    worker = null;
+                if (nearestParent1 == null && nearestParent2 == null && worker == null)
                 {
-                    UserInspector userInspector = slot1.AttachComponent<UserInspector>();
-                    userInspector.ViewUser.Target = nearestParent2;
-                    Stream nearestParent3 = target.FindNearestParent<Stream>();
-                    if (nearestParent3 != null)
-                    {
-                        userInspector.ViewGroup.Value = UserInspector.View.Streams;
-                        userInspector.ViewStreamGroup.Value = nearestParent3.GroupIndex;
-                    }
-                    else if (target.FindNearestParent<UserComponent>() != null)
-                        userInspector.ViewGroup.Value = UserInspector.View.Components;
-                    else
-                        userInspector.ViewGroup.Value = UserInspector.View.User;
-                }
-                if (slot2 != null)
-                {
-                    slot1.CopyTransform(slot2);
-                    //slot2.Position_Field.TweenTo(slot2.LocalPosition + (slot2.LocalRotation * float3.Forward * 0.1f), 0.2f);
-                    slot1.Position_Field.TweenTo(slot1.LocalPosition + (slot1.LocalRotation * float3.Backward * DoNotRunAwaySceneInspecter.Distance), 0.2f);
+                    UniLog.Warning("The target is neither on a Slot nor on an User.\n" + target.ParentHierarchyToString());
                 }
                 else
-                    slot1.PositionInFrontOfUser(new float3?(float3.Backward));
-            }
+                {
+                    Slot slot1 = target.World.LocalUserSpace.AddSlot("Inspector");
+                    Slot slot2 = source?.GetComponentInParents<SceneInspector>()?.Slot ?? source?.GetComponentInParents<UserInspector>()?.Slot;
+                    if (worker != null)
+                        WorkerInspector.Create(slot1, worker);
+                    else if (nearestParent1 != null)
+                    {
+                        SceneInspector sceneInspector = slot1.AttachComponent<SceneInspector>();
+                        sceneInspector.Root.Target = nearestParent1;
+                        sceneInspector.ComponentView.Target = nearestParent1;
+                    }
+                    else if (nearestParent2 != null)
+                    {
+                        UserInspector userInspector = slot1.AttachComponent<UserInspector>();
+                        userInspector.ViewUser.Target = nearestParent2;
+                        Stream nearestParent3 = target.FindNearestParent<Stream>();
+                        if (nearestParent3 != null)
+                        {
+                            userInspector.ViewGroup.Value = UserInspector.View.Streams;
+                            userInspector.ViewStreamGroup.Value = nearestParent3.GroupIndex;
+                        }
+                        else if (target.FindNearestParent<UserComponent>() != null)
+                            userInspector.ViewGroup.Value = UserInspector.View.Components;
+                        else
+                            userInspector.ViewGroup.Value = UserInspector.View.User;
+                    }
+                    if (slot2 != null)
+                    {
+                        slot1.CopyTransform(slot2);
+                        if (Distance > 0.0)
+                        {
+                            slot1.Position_Field.TweenTo(slot1.LocalPosition + (slot1.LocalRotation * float3.Backward * Distance), 0.2f);
+                        }
+                        else
+                        {
+                            slot2.Position_Field.TweenTo(slot2.LocalPosition + (slot2.LocalRotation * float3.Backward * Distance), 0.2f);
+                        }
+                    }
+                    else
+                        slot1.PositionInFrontOfUser(new float3?(float3.Backward));
+                }
 
-            return false;
+                return false;
+            }
         }
     }
 }
